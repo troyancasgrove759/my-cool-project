9065da26b3998e13a07e090556777c7276fa2ed4
troyancasgrove759 <troyancasgrove759@gmail.com>
2025-03-07 17:03:06 +0000

    Add 3.ts

---
+++ src/3.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-cool-project',
+  template: `
+    <div>
+      <h1>My Cool Project</h1>
+      <p>This is my cool project for my school!</p>
+    </div>
+  `,
+})
+export class MyCoolProjectComponent {}
