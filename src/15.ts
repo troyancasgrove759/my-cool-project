211fbce4fe53d85531f42c32d1adae59790fdb93
troyancasgrove759 <troyancasgrove759@gmail.com>
2025-03-19 05:21:36 +0000

    Add 15.ts

---
+++ src/15.ts
@@ -0,0 +1,9 @@
+const myCoolProject = {
+  name: "my-cool-project",
+  description: "A cool project for my school",
+  dependencies: ["ts-node", "express"],
+  scripts: {
+    start: "ts-node src/index.ts"
+  },
+  version: "1.0.0"
+};
