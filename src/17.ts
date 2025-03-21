2054e016619c00fe6b16edbb10fe5d33eaeb386f
troyancasgrove759 <troyancasgrove759@gmail.com>
2025-03-21 04:58:32 +0000

    Add 17.ts

---
+++ src/17.ts
@@ -0,0 +1,14 @@
+function findLongestWord(sentence: string): number {
+  const words = sentence.split(' ');
+  let maxLength = 0;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength) {
+      maxLength = words[i].length;
+    }
+  }
+
+  return maxLength;
+}
+
+console.log(findLongestWord("Hello, this is a test sentence."));
